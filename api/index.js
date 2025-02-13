b8b8e440020e3e75d7f076d6deb2b99bbb54fccb
MMB <mailadeyinka2020@gmail.com>
2025-02-13 15:45:12 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { error } from 'console';
+dotenv.config();
 const app = express();
 
-// server
-app.listen(3000,()=>{
-    console.log('server is running on port 3000')
+//connecting to the database
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error:", err);
+    });
+
+
+
+// server listining
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
 })
